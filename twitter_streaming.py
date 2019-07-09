5010d654241218846060bae91ea28c46b91fe95e
Manas Bhardwaj <manas1109bhardwaj@gmail.com>
2019-07-09 23:32:35 +0530

    Update twitter_streaming.py

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -3,10 +3,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API 
-access_token = "1133205265187131392-4h9hV3hdCIIBHJXsce2umHWdgNKnxR"
-access_token_secret = "cZgta6nL6pz5IVCdBf2oGvuYQrh5JRqrEpdeNGEbNxK9b"
-consumer_key = "Il4y1yMLiJAuO9ouqcXSZESYJ"
-consumer_secret = "ITJTqFApceDy2FfYz0vong4UK3ve4YbPBSTmwKpkp79Tqj2pkt"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 
 #This is a basic listener that just prints received tweets to stdout.
